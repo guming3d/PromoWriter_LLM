55042ea49fa8c6244412a21d0d7ec385859be23e
minggu <minggu@microsoft.com>
2024-08-16 16:30:24 +0800

    style: adjust column width in sidebar layout

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if not env_valid:
 else:
     # Left column - Input section
     with st.sidebar:
-        col1, col2 = st.columns([1, 3])
+        col1, col2 = st.columns([1, 5])
         with col1:
             st.logo(azure_logo)
         with col2:
